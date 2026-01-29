b91e27e063afa6d41c00c8b27df8aa9c6d606967
prathamppatel <prathamppatel@utexas.edu>
2026-01-29 06:46:25 +0000

    Comitting exercise2.py

---
+++ homework02/exercise2.py
@@ -0,0 +1,10 @@
+from Bio.Seq import Seq
+
+dna_sequence = Seq('GAACCGGGAGGTGGGAATCCGTCACATATGAGAAGGTATTTGCCCGATAA')
+
+g_count = dna_sequence.count('G')
+c_count = dna_sequence.count('C')
+
+gc_content = (g_count + c_count) / len(dna_sequence)
+
+print(f'The GC content of the sequence is {gc_content}.')
